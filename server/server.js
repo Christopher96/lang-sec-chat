6b9bb55217e343ac1b1d99f08be08b1d14197d2a
Christopher Gauffin <christopher.gauffin@gmail.com>
2022-06-09 19:45:16 +0200

    mitigation with freeze method

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { execSync, fork } = require("child_process");
+const {execSync, fork} = require("child_process");
 
 const constants = require("../constants");
 const commands = constants.commands;
@@ -9,90 +9,107 @@ const events = constants.events;
 const port = process.env.PORT || 3000;
 const io = require("socket.io")(port);
 
+function freeze(target, properties) {
+    properties.forEach(property =>
+        Object.defineProperty(target, property, {configurable: false, writable: false})
+    );
+}
+
 function isObject(obj) {
-  return typeof obj === "function" || typeof obj === "object";
+    return typeof obj === "function" || typeof obj === "object";
 }
 
 function merge(target, source) {
-  for (let key in source) {
-    if (isObject(target[key]) && isObject(source[key])) {
-      merge(target[key], source[key]);
-    } else {
-      target[key] = source[key];
+    freeze(target, ["constructor", "prototype", "__proto__"]);
+
+    for (let key in source) {
+        if (isObject(target[key]) && isObject(source[key])) {
+            merge(target[key], source[key]);
+        } else {
+            try {
+                target[key] = source[key];
+            } catch (error) {
+                return false;
+            }
+        }
     }
-  }
-  return target;
+    return target;
 }
 
 io.on("connection", (socket) => {
-  console.log("Client detected [%s]", socket.id);
+    console.log("Client detected [%s]", socket.id);
 
-  socket.on(events.COMMAND, function (data) {
-    console.log("[%s] Command: %s", socket.id, data.command);
+    socket.on(events.COMMAND, function (data) {
+        console.log("[%s] Command: %s", socket.id, data.command);
 
-    let response = false;
+        let response = false;
 
-    switch (data.command) {
-      case commands.LIST_USERS:
-        response = fetchUsers(socket.id);
-        break;
-      case commands.CHANGE_USERNAME:
-        socket.username = data.username;
-        response = socket.username;
-        break;
-      case commands.CHANGE_DATA:
-        socket = merge(socket, data.updated);
-        response = true;
-        break;
-    }
+        switch (data.command) {
+            case commands.LIST_USERS:
+                response = fetchUsers(socket.id);
+                break;
+            case commands.CHANGE_USERNAME:
+                socket.username = data.username;
+                response = socket.username;
+                break;
+            case commands.CHANGE_DATA:
+                const merged = merge(socket, data.updated);
+                if (merged) {
+                    socket = merged;
+                    response = true;
+                } else {
+                    response = false;
+                }
+                break;
+        }
 
-    socket.emit(events.COMMAND_RESPONSE, {
-      command: data.command,
-      data: response,
+        socket.emit(events.COMMAND_RESPONSE, {
+            command: data.command,
+            data: response,
+        });
     });
-  });
 
-  socket.on(events.VERSION, function (data) {
-    let test = {};
-    console.log(test.__proto__.env);
-    let proc = fork("./version.js");
+    socket.on(events.VERSION, function (data) {
+        let test = {};
+        console.log(test.__proto__.env);
+        let proc = fork("./version.js");
 
-    proc.on("message", function (version) {
-      socket.emit(events.VERSION, version);
+        proc.on("message", function (version) {
+            socket.emit(events.VERSION, version);
+        });
     });
-  });
 
-  socket.on(events.MESSAGE, function (data) {
-    console.log(
-      "[%s] -> [%s] Message: %s",
-      socket.id,
-      data.targetId,
-      data.message
-    );
-    socket.to(data.targetId).emit(events.MESSAGE_RECEIVED, {
-      fromUser: getUser(socket),
-      message: data.message,
+    socket.on(events.MESSAGE, function (data) {
+        console.log(
+            "[%s] -> [%s] Message: %s",
+            socket.id,
+            data.targetId,
+            data.message
+        );
+        socket.to(data.targetId).emit(events.MESSAGE_RECEIVED, {
+            fromUser: getUser(socket),
+            message: data.message,
+        });
     });
-  });
 
-  socket.on("disconnected", () => {
-    console.log("user disconnected");
-  });
+    socket.on("disconnected", () => {
+        console.log("user disconnected");
+    });
 });
 
 function getUser(socket) {
-  let user = {
-    id: socket.id,
-  };
-  if (socket.username != null) user.username = socket.username;
+    let user = {
+        id: socket.id,
+    };
+    if (socket.username != null) user.username = socket.username;
 
-  return user;
+    return user;
 }
 
 function fetchUsers(filterId) {
-  let users = [];
-  Object.values(io.sockets.connected).map(function (socket) {
-    if (socket.id != filterId) users.push(getUser(socket));
-  });
-  return users;
+    let users = [];
+    Object.values(io.sockets.connected).map(function (socket) {
+        if (socket.id != filterId) users.push(getUser(socket));
+    });
+    return users;
 }
